fd9ad1fb81030a7171f38c9213d0cce7d21b8f37
marekczapla <106765325+marekczapla@users.noreply.github.com>
2023-03-02 21:28:17 +0100

    Initial commit

---
+++ src/main/java/pl/marekczapla/windsurfersweatherforecastapi/WindsurfersWeatherForecastApiApplication.java
@@ -0,0 +1,13 @@
+package pl.marekczapla.windsurfersweatherforecastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurfersWeatherForecastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindsurfersWeatherForecastApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marekczapla/windsurfersweatherforecastapi/WindsurfersWeatherForecastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marekczapla.windsurfersweatherforecastapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurfersWeatherForecastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
